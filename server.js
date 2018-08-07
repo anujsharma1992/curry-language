89ceeddcf95a2e505622a8bb03d8f8bc3990b12b
kamall2015 <40353914+kamall2015@users.noreply.github.com>
2018-08-08 03:01:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,65 @@ app.listen(config.production.port, function () {
 
 
 
+// paste into common-helper
 
-global.sendgridHelper = require('sendgrid').mail;
-global.sendgridObj = require('sendgrid')('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');/* All config set to be in env file and then no need to hardcode it.*/
-global.ExternalService = require("./helpers/common-service");
+"use strict"
+let nodemailer = require('nodemailer');
+
+exports.mailGun = (type, email, params) => {
+    try {
+var transporter = nodemailer.createTransport({
+ service: 'gmail', 
+ auth: {
+        user: 'sharmakamal487@gmail.com',
+        pass: 'kamalmansi'
+    }
+});
+
+let htmlContentData = htmlContent(type,params)
+
+const mailOptions = {
+  from: 'no-reply@munch.com', // sender address
+  to: email, // list of receivers
+  subject: htmlContentData.subject, // Subject line
+  html: htmlContentData.htmlContent// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
+
+
+    } catch (e) {
+        console.log('sendgrid error', e);
+        return;
+    }
+}
+
+
+
+
+const htmlContent = (type,params) => {
+if (type==='forgot-password'){
+let newstr = '<p>Hi {name},</p> <p>Your system generated password is {password}. This is a system generated password so we requested you to changes password after login.<p>Thanks</p> <p> munch-app Team.</p>';
+            for (let key in params) {
+                if (params.hasOwnProperty(key)) {
+                     newstr = newstr.replace(key, params[key]);
+                }
+            }
+
+return {'htmlContent':newstr,'subject':'Forgot Password!!'};
+}
+
+}
+
+
+/*
+ "nodemailer": "^4.6.4",
+    "nodemailer-smtp-transport": "^2.7.4",
+    
+// global.ExternalService = require("./helpers/common-service");
+/*
